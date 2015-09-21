58ec5b0ff124998bb31235456531c503b5c8771b
Roy Clarkson <rclarkson@pivotal.io>
2015-09-21 17:38:53 -0500

    Enable Spring Boot test support (we need tests)

---
+++ src/test/java/io/pivotal/springcloud/ssl/CloudFoundryCertificateTrusterTest.java
@@ -0,0 +1,12 @@
+package io.pivotal.springcloud.ssl;
+
+import org.junit.Test;
+
+public class CloudFoundryCertificateTrusterTest {
+
+	@Test
+	public void nothing() {
+		// TODO: add more tests
+	}
+
+}
