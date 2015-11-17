f9804fb3318cdc3a2c6e5f896082ba5766e7fc6f
Roy Clarkson <rclarkson@pivotal.io>
2015-11-17 08:12:14 -0600

    Add license information

--- src/main/java/io/pivotal/springcloud/ssl/SslCertificateTruster.java
+++ src/main/java/io/pivotal/springcloud/ssl/SslCertificateTruster.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.pivotal.springcloud.ssl;
 
 import java.io.File;
@@ -25,9 +41,9 @@ import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
 /**
- * 
+ *
  * Utility for getting the JVM to trust an untrusted certificate
- * 
+ *
  * @author wtran@pivotal.io
  *
  */
@@ -52,7 +68,7 @@ public class SslCertificateTruster {
 	 * Performs an SSL handshake with the given host and port, and if the JVM
 	 * does not trust the certificate presented during SSL handshake, return the
 	 * certificate chain, otherwise return null.
-	 * 
+	 *
 	 * @param host
 	 * @param port
 	 * @param timeout
@@ -107,7 +123,7 @@ public class SslCertificateTruster {
 	 * the untrusted certificate to the JVM's currently used truststore. The
 	 * change is made to a copy of the truststore, and the JVM can use the new
 	 * copy via system property "javax.net.ssl.trustStore".
-	 * 
+	 *
 	 * @param host
 	 * @param port
 	 * @param timeout
@@ -128,7 +144,7 @@ public class SslCertificateTruster {
 	 * Append the certificate to the JVM's currently used truststore. The change
 	 * is made to a copy of the truststore, and the JVM can use the new copy via
 	 * system property "javax.net.ssl.trustStore".
-	 * 
+	 *
 	 * @param chain
 	 * @throws NoSuchAlgorithmException
 	 * @throws KeyStoreException
